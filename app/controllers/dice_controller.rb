c151963f5a497146949d4042441f6f2e4b02bd5f
Ashanti Hatchett <ahatchett02@gmail.com>
2024-09-20 19:28:09 +0000

    updated dice page to make it dynamic

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -4,6 +4,12 @@ class DiceController < ApplicationController
   end
 
   def dice_roll
+    @rolls = []
+    
+    50.times do
+      die = rand(1..6)
+      @rolls.push(die)
+    end
     render(template: "dice_templates/dice_roll")
   end
 end
