7b73918390919ded7b4ed6dd3cf4a9ed41c2fe59
Ashanti Hatchett <ahatchett02@gmail.com>
2024-09-20 19:43:31 +0000

    fixed dice page to display correct amount of bullet points

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -4,12 +4,16 @@ class DiceController < ApplicationController
   end
 
   def dice_roll
+    @number_of_dice = params.fetch(:number_of_dice).to_i
+    @sides = params.fetch(:how_many_sides).to_i
+
     @rolls = []
-    
-    50.times do
-      die = rand(1..6)
+
+    @number_of_dice.times do
+      die = rand(1..@sides)
       @rolls.push(die)
     end
+    
     render(template: "dice_templates/dice_roll")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   get("/", controller: "dice", action: "menu")
 
-  get("/dynamic/:number_of_dice/:how_many_sides", controller: "dice", action: "dice_roll")
+  get("/dice/:number_of_dice/:how_many_sides", controller: "dice", action: "dice_roll")
 
 end
